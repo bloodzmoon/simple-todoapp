6e1410ec60f852769d90a93ef5bccac1cc07c586
61010497 <61010497@kmitl.ac.th>
2020-02-02 19:27:02 +0700

    basic local storage

---
+++ src/components/TodoForm.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import uuid from 'uuid';
+
+function TodoForm({ addTodo }) {
+  const [todo, setTodo] = useState({
+    id: '',
+    task: '',
+    done: false
+  });
+  
+  function handleInputChange(e) {
+    setTodo({ ...todo, task: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (todo.task.trim()) {
+      addTodo({ ...todo, id: uuid.v4() })
+      setTodo({ ...todo, task: '' });
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="task"
+        type="text"
+        value={todo.task}
+        onChange={handleInputChange}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default TodoForm;
